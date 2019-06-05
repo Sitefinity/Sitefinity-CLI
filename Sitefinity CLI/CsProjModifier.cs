bed6fed593bbd808763b8472b73cb73d8f3a8649
Mario Yordanov <myordano@progress.com>
2019-06-05 15:54:17 +0300

    added class to handle the csproj modification

---
+++ Sitefinity CLI/CsProjModifier.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Sitefinity_CLI
+{
+    internal class CsProjModifier
+    {
+        private const string ItemGroupElem = "ItemGroup";
+        private const string CompileElem = "Compile";
+        private const string IncludeProperty = "Include";
+
+        private readonly string _xmlFilePath;
+        private readonly XDocument _doc;
+
+        public CsProjModifier(string xmlFilePath)
+        {
+            _xmlFilePath = xmlFilePath;
+            _doc = XDocument.Load(_xmlFilePath);
+        }
+
+        public void AddFileToCsproj(string filePath)
+        {
+            if (!Validate())
+            {
+                return;
+            }
+
+            XElement parent = GetFirstParentWithCompileElements();
+            XElement elem = GetElemByAttributeValue(parent, filePath);
+
+            if (elem == null)
+            {
+                elem = new XElement(CompileElem, new XAttribute(IncludeProperty, filePath));
+                parent.Add(elem);
+            }
+        }
+
+        public void RemoveFileFromCsProj(string filePath)
+        {
+            if (!Validate())
+            {
+                return;
+            }
+
+            XElement parent = GetFirstParentWithCompileElements();
+            XElement elem = GetElemByAttributeValue(parent, filePath);
+
+            if (elem != null)
+            {
+                elem.Remove();
+            }
+        }
+
+        public void SaveDocument()
+        {
+            _doc.Save(_xmlFilePath);
+        }
+
+        private XElement GetFirstParentWithCompileElements()
+        {
+            XElement parent = _doc.Descendants()
+                    .FirstOrDefault(x => x.Name.ToString().EndsWith(ItemGroupElem)
+                            && x.Descendants().Any(desc => desc.Name.ToString().EndsWith(CompileElem)));
+
+            return parent;
+        }
+
+        private XElement GetElemByAttributeValue(XElement parent, string value)
+        {
+            XElement elem = parent.Descendants()
+                .Where(x => x.Attribute(IncludeProperty).Value == value)
+                .FirstOrDefault();
+
+            return elem;
+        }
+
+        private bool Validate()
+        {
+            if (string.IsNullOrEmpty(_xmlFilePath) || _doc == null)
+            {
+                Utils.WriteLine("Missing xml file path or document is null", ConsoleColor.Red);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
