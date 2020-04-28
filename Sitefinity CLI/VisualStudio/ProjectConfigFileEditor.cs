0c2ce59e96ba751e7d26424df3479a865b9faaac
PROGRESS\sruzmano <stoyan.ruzmanov@gmail.com>
2020-04-28 14:23:12 +0300

    Revert configs for non sitefinity projects  after nuget update

---
+++ Sitefinity CLI/VisualStudio/ProjectConfigFileEditor.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+namespace Sitefinity_CLI.VisualStudio
+{
+    internal class ProjectConfigFileEditor : IProjectConfigFileEditor
+    {
+        public string GetProjectConfigPath(string projectLocation)
+        {
+            if (string.IsNullOrEmpty(projectLocation))
+                return null;
+
+            var projectDirectory = Path.GetDirectoryName(projectLocation);
+
+            var webConfigPath = Path.Combine(projectDirectory, "web.config");
+            if (File.Exists(webConfigPath))
+                return webConfigPath;
+
+            var appConfigPath = Path.Combine(projectDirectory, "app.config");
+            if (File.Exists(appConfigPath))
+                return appConfigPath;
+
+            return null;
+        }
+    }
+}
