5ea80a074e39e17c5a7fad4e7c4d7c12f6c94c7e
Stoyan Ruzmanov <stoyan.ruzmanov@gmail.com>
2020-09-08 15:52:41 +0300

    Templates 13.1 Messages for upgrade command (#68)
    * Added templates for 13.1
    
    * Fixed typo in the upgrade warning message. Added message for "rebuild" after a succesful upgrade
    
    * FIxed reference for Telerik.Sitefinity and Telerik.Sitefinity.Services.Events in the .csproj template

--- Sitefinity CLI/Constants.cs
+++ Sitefinity CLI/Constants.cs
@@ -61,7 +61,8 @@ namespace Sitefinity_CLI
         public const string SolutionNotReadable = "Unable to read solution";
         public const string NoProjectsFoundToUpgradeWarningMessage = "No projects with Sitefinity references found to upgrade.";
         public const string AcceptLicenseNotification = "Do you accept the terms and conditions";
-        public const string UpgradeWarning = "Make sure to have your project under source controll. Currently there is no revert mechanism in the upgrade tool. The upgrade will launch visual studio instanse in order to execute nuget upgrade. DO NOT CLOSE the opened visual studio. This will stop the upgrade. Do you want to continue?";
+        public const string UpgradeWarning = "Make sure to have your project under source control. Currently there is no revert mechanism in the upgrade tool. The upgrade will launch visual studio instanse in order to execute nuget upgrade. DO NOT CLOSE the opened visual studio. This will stop the upgrade. Do you want to continue?";
+        public const string UpgradeSuccessMessage = "Successfully updated '{0}' to version '{1}'. Make sure to REBUILD your solution before starting up the site!";
 
         // Success messages
         public const string ConfigFileCreatedMessage = "Configuration file created successfully! Path: \"{0}\"";
