333a54f41d8bda99958b88fd3a4a46bfcd0ae5fe
PROGRESS\sruzmano <stoyan.ruzmanov@gmail.com>
2020-12-09 09:00:03 +0200

    Templates for 13.2

---
+++ Sitefinity CLI/Templates/13.2/ResourcePackage/Bootstrap4/MVC/Scripts/Navigation/pills.js
@@ -0,0 +1,30 @@
+﻿(function () {
+    /* Polyfills */
+
+    if (window.NodeList && !NodeList.prototype.forEach) {
+        NodeList.prototype.forEach = Array.prototype.forEach;
+    }
+
+    document.addEventListener('DOMContentLoaded', function () {
+        document.querySelectorAll('.expandable-node').forEach(function (x) {
+            x.addEventListener('click', function (e) {
+                if (window.DataIntelligenceSubmitScript && e.currentTarget.hasAttribute('aria-expanded') === 'false') {
+                    DataIntelligenceSubmitScript._client.sentenceClient.writeSentence({
+                        predicate: "Toggle navigation",
+                        object: e.currentTarget.innerText.trim(),
+                        objectMetadata: [{
+                            'K': 'PageTitle',
+                            'V': document.title
+                        },
+                        {
+                            'K': 'PageUrl',
+                            'V': location.href
+                        }
+                        ]
+                    });
+                }
+            });
+        });
+    });
+
+}());
