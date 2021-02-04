e2c9fa6eeabb18a99d324206383cbf35f843624b
PROGRESS\sruzmano <stoyan.ruzmanov@gmail.com>
2021-02-04 15:16:29 +0200

    Added logic to Set the execution policy to unrestricted for the current process. AdmnRights validator added

---
+++ Sitefinity CLI/Commands/Validators/AdminRightsValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Principal;
+
+namespace Sitefinity_CLI.Commands.Validators
+{
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = false)]
+    internal class AdminRightsValidator : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var isAdmin = (new WindowsPrincipal(WindowsIdentity.GetCurrent()))
+                           .IsInRole(WindowsBuiltInRole.Administrator);
+
+            if (!isAdmin)
+            {
+                return new ValidationResult("You must be administrator in order to run this command!");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
