6b5f49ed3631f5eb6523de9c7a348e7250687b3d
Stoyan Ruzmanov <sruzmano@progress.com>
2023-08-30 06:59:07 +0300

    Added WEbP and Dam libs

--- Sitefinity CLI/Commands/UpgradeCommand.cs
+++ Sitefinity CLI/Commands/UpgradeCommand.cs
@@ -486,7 +486,7 @@ namespace Sitefinity_CLI.Commands
 
         private readonly HttpClient httpClient;
 
-        private readonly ICollection<string> allowedAdditionalPackagesIds = new List<string>() { "Progress.Sitefinity.Cloud", "Progress.Sitefinity.MOVEit", "Telerik.Sitefinity.DAM" };
+        private readonly ICollection<string> allowedAdditionalPackagesIds = new List<string>() { "Progress.Sitefinity.Cloud", "Progress.Sitefinity.MOVEit", "Progress.Sitefinity.Libraries.Cloudinary", "Progress.Sitefinity.Libraries.Frontify", "Progress.Sitefinity.Libraries.WebP" };
 
         private const string SfAllNugetUrl = "https://nuget.sitefinity.com/api/packages/ids?page=1&contains=Telerik.Sitefinity.All";
     }
