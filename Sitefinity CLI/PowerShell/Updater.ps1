e7b9f69fcfeffd41a3dee69cc7725ac685befe82
Georgi Georgiev <gigeorgiev@progress.com>
2024-05-22 16:52:30 +0300

    Add condition in case Addon projects are not using the full Sitefinity.All package

--- Sitefinity CLI/PowerShell/Updater.ps1
+++ Sitefinity CLI/PowerShell/Updater.ps1
@@ -64,7 +64,7 @@ Try
 
 		if($RemoveDeprecatedPackages -eq "True") 
 		{
-			$sfPackageVersion = ($packages | Where-Object { $_.name -eq "Telerik.Sitefinity.All" }).Version
+			$sfPackageVersion = ($packages | Where-Object { $_.name -eq "Telerik.Sitefinity.All" -or $_.name -eq "Progress.Sitefinity"}).Version
 
 			if($null -ne $sfPackageVersion)
 			{
