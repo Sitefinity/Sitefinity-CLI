a67d2d43a6558fa72d5c88354e7d73e5bd973206
Mihail Shterev <93082689+Shredrox@users.noreply.github.com>
2024-07-15 13:08:56 +0300

    Add unit tests for Create command

---
+++ Sitefinity CLI.Tests/CreateCommandTests/CreateCommandSut.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Logging;
+using Sitefinity_CLI.Commands;
+using Sitefinity_CLI.PackageManagement;
+using System.Threading.Tasks;
+
+namespace Sitefinity_CLI.Tests.CreateCommandTests
+{
+    internal class CreateCommandSut : CreateCommand
+    {
+        public CreateCommandSut(
+            ILogger<CreateCommand> logger, 
+            IVisualStudioWorker visualStudioWorker, 
+            IDotnetCliClient dotnetCliClient)
+            : base(logger, visualStudioWorker, dotnetCliClient)
+        {
+        }
+
+        public async Task Execute()
+        {
+            await this.ExecuteCreate();
+        }
+    }
+}
