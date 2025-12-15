672e533c1a94dedc4c57e5a482b3b95fd186e92b
Pavel Stoyanov <85177323+lilpaf@users.noreply.github.com>
2025-12-15 15:40:40 +0200

    Update nuget version and change deprecated flags  (#371)

--- Sitefinity CLI/PackageManagement/Implementations/NuGetCliClient.cs
+++ Sitefinity CLI/PackageManagement/Implementations/NuGetCliClient.cs
@@ -1,7 +1,7 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Net;
-using System.Net.Http;
 using Microsoft.Extensions.Logging;
 using Sitefinity_CLI.PackageManagement.Contracts;
 
@@ -16,12 +16,12 @@ namespace Sitefinity_CLI.PackageManagement.Implementations
 
         public void InstallPackage(string packageId, string version, string solutionDirectory, string nugetConfigPath)
         {
-            this.RunProcess($"install \"{packageId}\" -Version {version} -SolutionDirectory \"{solutionDirectory}\" -NoCache -ConfigFile \"{nugetConfigPath}\" -prerelease");
+            this.RunProcess($"install \"{packageId}\" -Version {version} -SolutionDirectory \"{solutionDirectory}\" {NoHttpCacheFlag} -ConfigFile \"{nugetConfigPath}\" -prerelease");
         }
 
         public void Restore(string solutionFilePath)
         {
-            this.RunProcess($"restore \"{solutionFilePath}\" -NoCache");
+            this.RunProcess($"restore \"{solutionFilePath}\" {NoHttpCacheFlag}");
         }
 
         private void RunProcess(string arguments)
@@ -63,7 +63,10 @@ namespace Sitefinity_CLI.PackageManagement.Implementations
 
         private void EnsureNugetExecutable(string nugetFileLocation)
         {
-            if (!File.Exists(nugetFileLocation))
+            FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(nugetFileLocation);
+            Version nugetVersion = new Version(fileVersionInfo.FileVersion);
+
+            if (!File.Exists(nugetFileLocation) || nugetVersion < NuGetExeVersion)
             {
                 using (var client = new WebClient())
                 {
@@ -74,6 +77,8 @@ namespace Sitefinity_CLI.PackageManagement.Implementations
 
         private readonly ILogger<NuGetCliClient> logger;
         private const string NuGetExeFileName = "nuget.exe";
-        private const string NuGetExeDownloadUrl = "https://dist.nuget.org/win-x86-commandline/v6.11.1/nuget.exe";
+        private const string NuGetExeDownloadUrl = "https://dist.nuget.org/win-x86-commandline/v7.0.1/nuget.exe";
+        private static readonly Version NuGetExeVersion = new Version("7.0.1.1");
+        private const string NoHttpCacheFlag = "-NoHttpCache";
     }
 }
